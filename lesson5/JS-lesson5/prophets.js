d6d22f9fd9760920f59b0c27f08b7cebf67abba3
svpontes <svpontes2015@gmail.com>
2022-03-29 14:30:40 +0100

    commit

---
+++ lesson5/JS-lesson5/prophets.js
@@ -0,0 +1,31 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jsonObject) {
+        console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const prophets = jsonObject["prophets"]
+        for (let i=0; i < prophets.length; i++) {
+          let card = document.createElement('section');
+          let h2 = document.createElement('h2');
+          h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+          card.appendChild(h2);
+          document.querySelector('div.cards').appendChild(card);
+          let h3 = document.createElement("h3");
+          h3.textContent = "Date of Birth: " + prophets[i].birthdate
+          card.appendChild(h3)
+          let h4 = document.createElement("h4");
+          h4.textContent = "Place of Birth: " + prophets[i].birthplace
+          card.appendChild(h4)
+          let img = document.createElement("img")
+          img.setAttribute("src", prophets[i].imageurl)
+          img.setAttribute("alt", prophets[i].name +" "+ prophets[i].lastname + " -" + i+1)
+          card.appendChild(img)
+
+        }
+          
+   
+          
+        });
